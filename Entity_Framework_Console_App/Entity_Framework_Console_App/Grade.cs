9451810290990263ade334b7c26fe77adb7d3493
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-08-12 18:54:09 -0700

    added entity framework console app

---
+++ Entity_Framework_Console_App/Entity_Framework_Console_App/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Console_App
+{
+    internal class Grade
+    {
+        public int GradeId { get; set; }
+        public double GradePoint { get; set; }
+        public string Section { get; set; }
+    }
+}
---
+++ Entity_Framework_Console_App/Entity_Framework_Console_App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = new Student()
+                {
+                    Name = "Bilbo"
+                };
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Program Over. Hit enter to Exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
