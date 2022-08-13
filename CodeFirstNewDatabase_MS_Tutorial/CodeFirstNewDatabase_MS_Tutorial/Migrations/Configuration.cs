f7108a0896597c901d93dc488e6623e4bec859cb
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-08-12 17:21:00 -0700

    added code first

---
+++ CodeFirstNewDatabase_MS_Tutorial/CodeFirstNewDatabase_MS_Tutorial/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeFirstNewDatabase_MS_Tutorial.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstNewDatabase_MS_Tutorial.BlogContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstNewDatabase_MS_Tutorial.BlogContext";
+        }
+
+        protected override void Seed(CodeFirstNewDatabase_MS_Tutorial.BlogContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CodeFirstNewDatabase_MS_Tutorial/CodeFirstNewDatabase_MS_Tutorial/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabase_MS_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogContext()) //New One Each Time Run
+            {
+                Console.Write("Name the new Blog:");
+                var blog = new Blog { Name = Console.ReadLine() };
+
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from eachBlog in db.Blogs
+                            orderby eachBlog.Name
+                            select eachBlog;
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+            }
+            Console.WriteLine("\nProgram Over");
+            Console.ReadLine();
+        }// END MAIN
+
+
+        
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+        public string URL { get; set; }
+    }
+
+    public class Post //pogo classes
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; } //lazy loading with virtual
+    }
+
+    public class User
+    {
+        [Key] //this data Annotation sets the following field as the primary key in the final database.
+        public string UserName { get; set; } // primary key should be "UserId", so EF can find it
+        public string DisplayName { get; set; }
+
+    }
+
+    public class BlogContext : DbContext { //All classes included in Model
+    public DbSet<Blog> Blogs { get; set; } //allows us to query instances of these types
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+            //base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+}
